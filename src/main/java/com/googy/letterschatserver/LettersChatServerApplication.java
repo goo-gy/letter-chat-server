357190786ad0d5ff6cf1fb4d316abdfcf8a9bbdb
goo-gy <lkukh17@naver.com>
2022-12-27 19:52:19 +0900

    init

---
+++ src/main/java/com/googy/letterschatserver/LettersChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.googy.letterschatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettersChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettersChatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/googy/letterschatserver/LettersChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googy.letterschatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettersChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
