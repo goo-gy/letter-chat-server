c46856fc10ebb60069c783968d1188182fef9a53
goo-gy <lkukh17@naver.com>
2023-01-03 23:26:54 +0900

    fix : kafka 사용
    - FrontEnd에서 메세지를 받으면 Kafka에 Publish
    - Consumer에서 Kafka의 Data를 읽어 FrontEnd로 전송

---
+++ src/main/java/com/googy/letterschatserver/kafka/KafkaConstant.java
@@ -0,0 +1,6 @@
+package com.googy.letterschatserver.kafka;
+
+public class KafkaConstant {
+    public static final String TOPIC_CHAT = "chat";
+    public static final String GROUP_ID = "letters";
+}
