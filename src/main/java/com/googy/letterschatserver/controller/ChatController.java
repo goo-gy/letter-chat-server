ca7a908945a2c9ee094f3dae8c78d93be485c1c0
goo-gy <lkukh17@naver.com>
2023-01-05 07:50:23 +0900

    fix : remove kafka

--- src/main/java/com/googy/letterschatserver/controller/ChatController.java
+++ src/main/java/com/googy/letterschatserver/controller/ChatController.java
@@ -1,20 +1,21 @@
 package com.googy.letterschatserver.controller;
 
 import com.googy.letterschatserver.domain.ChatDto;
-import com.googy.letterschatserver.kafka.KafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class ChatController {
+
     @Autowired
-    private KafkaProducer producer;
+    private SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/chat") // /pub/chat
     public void publishChat(ChatDto chatDto) {
-        producer.sendMessage(chatDto);
+        simpMessagingTemplate.convertAndSend("/sub/chat/" + chatDto.getChannel(), chatDto);
     }
 }
--- src/main/java/com/googy/letterschatserver/kafka/KafkaConstant.java
+++
@@ -1,6 +0,0 @@
-package com.googy.letterschatserver.kafka;
-
-public class KafkaConstant {
-    public static final String TOPIC_CHAT = "chat";
-    public static final String GROUP_ID = "letters";
-}
